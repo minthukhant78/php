bd49df6c1d61ba562e94559e14a988f1f7ce38cd
minthukhant78 <minthukhant2001@outlook.com>
2023-07-19 12:18:27 +0630

    19/7/23

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+    // $x = 4 ;
+
+    // switch ($x) {
+    //     case $x < 5 :
+    //         echo 'X is less than 5';
+    //         break;
+        
+    //     default:
+    //     echo 'X is greater than 5';
+    //         break;
+    // }
+
+    // if ("null") {
+
+    //     echo "Go to teashop";
+        
+    //     }
+
+//     $even = false;
+
+// if ($even = true) {
+
+// echo "it is even number";
+// 
+// };
+
+// $total_mark = 50 ;
+// if ($total_mark >= 150) {
+//     echo 'distintion';
+
+// }else if ( $total_mark < 50 ){
+//     echo 'fail';
+// }else {
+//     echo 'pass';
+// }
+
+$ch = "f";
+if ($ch >= "A" && $ch <= "Z"){
+    echo $ch;
+}
+?>
